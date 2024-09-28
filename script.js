0c7cd8ee05f064652f1d5147df0e7896448dc78d
Amanshaikh160 <166513907+Amanshaikh160@users.noreply.github.com>
2024-09-28 07:40:06 -0700

    table

---
+++ script.js
@@ -0,0 +1,20 @@
+// let food = ["fries","burger","pizza","beef"]
+let num = Number(prompt ("Enter Number"))
+if (num == 0 && num == "") {
+    num = 5
+}
+for( let i = 1; i <11  ; i++){
+    console.log(num,"x" ,i ,"=", num*i);
+}
+
+
+// console.log(food);
+
+// food.push("chicken")
+// console.log(food);
+
+// food.pop();
+// console.log(food);
+
+// let delectitem = food.pop();
+// console.log(delectitem);
